56f488bff5bf5b4d153e822c7a7f1411142cc318
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2024-06-10 23:24:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,43 +3,36 @@ import json
 import os
 import shutil
 
-
-def clear_dist_except(exceptions):
-    for item in os.listdir('dist'):
-        item_path = os.path.join('dist', item)
-        if item not in exceptions:
-            if os.path.isdir(item_path):
-                shutil.rmtree(item_path)
-            else:
-                os.remove(item_path)
-
-
 def main():
-    html = '<html><head><title>Droplet - URL Shortning Service</title><link rel="icon" href="dist/img/favicon.ico" type="image/x-icon"><meta http-equiv="refresh" content="0;url={url}" /></head><body><p>Redirecting...</p></body></html>'
+    html = '<html><head><title>Droplet - URL Shortening Service</title><link rel="icon" href="dist/img/favicon.ico" type="image/x-icon"><meta http-equiv="refresh" content="0;url={url}" /></head><body><p>Redirecting...</p></body></html>'
 
     with open('links.json') as f:
         links = json.load(f)
 
-    # Create 'dist' directory if it doesn't exist
-    if not os.path.exists('dist'):
-        os.mkdir('dist')
+    # Exclude these files from deletion
+    exclude_files = ['example.html']
 
-    # Clear 'dist' directory except specified exceptions
-    exceptions = ['example.html']  # Add the subfolders you want to keep here
-    clear_dist_except(exceptions)
+    # Remove files in 'dist' directory except for the excluded files
+    if os.path.exists('dist'):
+        for item in os.listdir('dist'):
+            item_path = os.path.join('dist', item)
+            if item not in exclude_files:
+                if os.path.isfile(item_path) or os.path.islink(item_path):
+                    os.unlink(item_path)
+                elif os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
 
-    # Create or overwrite the CNAME file
+    os.makedirs('dist', exist_ok=True)
+
     with open('dist/CNAME', 'w') as f:
         f.write('drp.lt')
 
-    # Create or overwrite the redirect files
     for link in links:
         html_document = html.format(url=link['url'])
         file_path = f"dist/{link['name']}.html"
 
         with open(file_path, 'w') as f:
             f.write(html_document)
-
 
 if __name__ == "__main__":
     main()
