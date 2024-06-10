fbb5d0ab598485b8642474ba94a455a733d9e626
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2024-06-11 00:03:31 +0530

    Update python-app.yml

---
+++ main.py
@@ -0,0 +1,26 @@
+#Zandora Edge Pvt. Ltd.
+import json
+import os
+import shutil
+
+def main():
+    html = '<html><head><title>Droplet - URL Shortening Service</title><link rel="icon" href="dist/img/favicon.ico" type="image/x-icon"><meta http-equiv="refresh" content="0;url={url}" /></head><body><p>Redirecting...</p></body></html>'
+
+    with open('links.json') as f:
+        links = json.load(f)
+
+    shutil.rmtree('dist', ignore_errors=True)
+    os.mkdir('dist')
+
+    with open('dist/CNAME', 'w') as f:
+        f.write('drp.lt')
+
+    for link in links:
+        html_document = html.format(url=link['url'])
+        file_path = f"dist/{link['name']}.html"
+
+        with open(file_path, 'w') as f:
+            f.write(html_document)
+
+if __name__ == "__main__":
+    main()
