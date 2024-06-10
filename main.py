a7ab960979c56e4df77d0f05f849ab655532e3ee
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2024-06-10 23:10:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ import os
 import shutil
 
 
+def clear_dist_except(exceptions):
+    for item in os.listdir('dist'):
+        item_path = os.path.join('dist', item)
+        if item not in exceptions:
+            if os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+            else:
+                os.remove(item_path)
+
+
 def main():
     html = '<html><head><title>Droplet - URL Shortning Service</title><link rel="icon" href="dist/img/favicon.ico" type="image/x-icon"><meta http-equiv="refresh" content="0;url={url}" /></head><body><p>Redirecting...</p></body></html>'
 
     with open('links.json') as f:
         links = json.load(f)
 
-    # Check if 'dist' directory exists, create if it doesn't
+    # Create 'dist' directory if it doesn't exist
     if not os.path.exists('dist'):
         os.mkdir('dist')
+
+    # Clear 'dist' directory except specified exceptions
+    exceptions = ['example.html', 'dist']  # Add the subfolders you want to keep here
+    clear_dist_except(exceptions)
 
     # Create or overwrite the CNAME file
     with open('dist/CNAME', 'w') as f:
