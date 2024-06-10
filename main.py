b7fda0d4966fa5074bc8715cf07590d9895a9425
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2024-06-11 00:01:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,8 @@ def main():
     with open('links.json') as f:
         links = json.load(f)
 
-    # Exclude these files from deletion
-    exclude_files = ['example.html']
-
-    # Remove files in 'dist' directory except for the excluded files
-    if os.path.exists('dist'):
-        for item in os.listdir('dist'):
-            item_path = os.path.join('dist', item)
-            if item not in exclude_files:
-                if os.path.isfile(item_path) or os.path.islink(item_path):
-                    os.unlink(item_path)
-                elif os.path.isdir(item_path):
-                    shutil.rmtree(item_path)
-
-    os.makedirs('dist', exist_ok=True)
+    shutil.rmtree('dist', ignore_errors=True)
+    os.mkdir('dist')
 
     with open('dist/CNAME', 'w') as f:
         f.write('drp.lt')
